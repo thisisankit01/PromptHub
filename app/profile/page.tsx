d4ee36666030c754333718600bea843376a4cd2b
Ankit Pandey <ankitechzz@gmail.com>
2023-06-17 21:20:46 +0530

    search implemented with errors

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -27,8 +27,24 @@ const MyProfile = () => {
     router.push(`/update-prompt?id=${post._id}`);
   };
 
-  const handleDelete = async () => {
-    console.warn("are you sure to delete the Prompt ?");
+  const handleDelete = async (post: any) => {
+    const hasConfirmed = confirm(
+      "Are you sure you want to delete this prompt?"
+    );
+
+    if (hasConfirmed) {
+      try {
+        await fetch(`/api/prompt/${post._id.toString()}`, {
+          method: "DELETE",
+        });
+
+        const filteredPosts = posts.filter((item) => item._id !== post._id);
+
+        setPosts(filteredPosts);
+      } catch (error) {
+        console.log(error);
+      }
+    }
   };
 
   return (
--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,34 +1,21 @@
 "use client";
 
-import {
-  ChangeEventHandler,
-  DetailedHTMLProps,
-  FormEvent,
-  HTMLAttributes,
-  useState,
-} from "react";
+import { DetailedHTMLProps, FormEvent, HTMLAttributes, useState } from "react";
 import PromptCard from "./PromptCard";
 import { useEffect } from "react";
 
-// interface PromptCardListTypes {
-//   data: Array<String>;
-//   handleTagClick: DetailedHTMLProps<
-//     HTMLAttributes<HTMLDivElement>,
-//     HTMLDivElement
-//   >;
-// }
 interface PromptCardProps {
-  data: Array<String>;
-  post: Array<String>;
+  data: Array<any>; // Update the data prop to accept an array of any type
+  post: any; // Update the post prop to accept any type
   handleTagClick: DetailedHTMLProps<
     HTMLAttributes<HTMLDivElement>,
     HTMLDivElement
   >;
 }
 
-const PromptCardList = ({ data, post, handleTagClick }: PromptCardProps) => {
+const PromptCardList = ({ data, handleTagClick }: PromptCardProps) => {
   return (
-    <div className="mt-16 prompt_layout flex justify-center">
+    <div className="mt-16 prompt_layout">
       {data.map((posts) => {
         return (
           <PromptCard
@@ -43,25 +30,52 @@ const PromptCardList = ({ data, post, handleTagClick }: PromptCardProps) => {
 };
 
 const Feed = () => {
-  const [searchText, setSearchText] = useState<string>("");
   const [posts, setPosts] = useState<Array<string>>([]);
 
-  const handleSearchChange = async (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    // setSearchText()
+  //search states
+  const [searchText, setSearchText] = useState<string>("");
+  const [searchTimeout, setSearchTimeout] = useState(null);
+  const [searchedResults, setSearchedResults] = useState([]);
+
+  const fetchPost = async () => {
+    const response = await fetch("/api/prompt");
+    const data = await response.json();
+    setPosts(data);
   };
 
   useEffect(() => {
-    const fetchPost = async () => {
-      const response = await fetch("/api/prompt");
-      const data = await response.json();
-      setPosts(data);
-    };
-
-    console.log(posts);
-
     fetchPost();
   }, []);
+
+  const filterPrompts = (searchText: any) => {
+    const regex = new RegExp(searchText, "i"); //The RegExp object is used for matching text with a pattern ,  'i' flag for case-insensitive search
+    return posts.filter((item) => {
+      regex.test(item.creator.username) || //The test() method returns true if it finds a match, otherwise false.
+        regex.test(item.tag) ||
+        regex.test(item.prompt) ||
+        regex.test(item.description);
+    });
+  };
+
+  const handleSearchChange = (e: FormEvent<HTMLInputElement>) => {
+    clearTimeout(searchTimeout);
+    setSearchText(e.currentTarget.value);
+
+    // debounce method
+    setSearchTimeout(
+      setTimeout(() => {
+        const searchResult = filterPrompts(e.target.value);
+        setSearchedResults(searchResult);
+      }, 500)
+    );
+  };
+
+  const handleTagClick = (tagName: any) => {
+    setSearchText(tagName);
+
+    const searchResult = filterPrompts(tagName);
+    setSearchedResults(searchResult);
+  };
 
   return (
     <section className="feed">
@@ -70,12 +84,21 @@ const Feed = () => {
           type="text"
           placeholder="Search for a tag or a username"
           value={searchText}
-          onChange={() => handleSearchChange}
+          onChange={handleSearchChange}
           required
           className="search_input peer"
         />
       </form>
-      <PromptCardList data={posts} handleTagClick={() => {}} />
+
+      {/* All Prompts */}
+      {searchText ? (
+        <PromptCardList
+          data={searchedResults}
+          handleTagClick={handleTagClick}
+        />
+      ) : (
+        <PromptCardList data={posts} handleTagClick={handleTagClick} />
+      )}
     </section>
   );
 };
